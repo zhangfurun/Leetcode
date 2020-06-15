1554f0722ce56e4923522db99c9a3d1253eadf27
zhangfurun <122674287@qq.com>
2020-06-15 14:26:57 +0800

    update 014 二分法

--- Leetcode解题/0014.longestCommonPrefix.playground/Contents.swift
+++ Leetcode解题/0014.longestCommonPrefix.playground/Contents.swift
@@ -1,32 +1,36 @@
 import UIKit
 
 var str = "Hello, playground"
-func longestCommonPrefix(_ strs: [String]) -> String {
-    var result = ""
-    if strs.count > 0 {
-        if let firstObjc = strs.first {
-            if strs.count > 1 {
-                
-                let numbers = strs.sorted()
-//                print(numbers)
-                let first = numbers.first!
-                let last = numbers.last!
-                
-                let lastArray = Array(last)
-                for (index, firstChar) in first.enumerated() {
-                    if lastArray[index] != firstChar {
-                        return result
-                    }
-                    
-                    result += String(firstChar)
-                }
-            } else {
-                result = firstObjc
-            }
-        }
-    }
-    return result
-}
+
+// sort + 首尾
+//func longestCommonPrefix(_ strs: [String]) -> String {
+//    var result = ""
+//    if strs.count > 0 {
+//        if let firstObjc = strs.first {
+//            if strs.count > 1 {
+//
+//                let numbers = strs.sorted()
+////                print(numbers)
+//                let first = numbers.first!
+//                let last = numbers.last!
+//
+//                let lastArray = Array(last)
+//                for (index, firstChar) in first.enumerated() {
+//                    if lastArray[index] != firstChar {
+//                        return result
+//                    }
+//
+//                    result += String(firstChar)
+//                }
+//            } else {
+//                result = firstObjc
+//            }
+//        }
+//    }
+//    return result
+//}
+
+
 
 
 //func longestCommonPrefix(_ strs: [String]) -> String {
@@ -54,6 +58,48 @@ func longestCommonPrefix(_ strs: [String]) -> String {
 //       return result
 //   }
 
+// 二分法
+func longestCommonPrefix(_ strs: [String]) -> String {
+    var result = ""
+    guard strs.count > 1 else {
+        return strs.first ?? ""
+    }
+    
+    let min_string = strs.sorted().first!
+    
+    var low = 0
+    var height = min_string.count
+    
+    while low < height {
+        let mid = low + (height - low + 1) / 2
+        if compareString(strs: strs, flag: mid) {
+            low = mid
+        } else {
+            height = mid - 1
+        }
+    }
+    
+    result = String(min_string.prefix(low))
+    
+    return result
+}
 
-let array = ["flowerdsadadada","glight","flight"]
+func compareString(strs: [String], flag: Int) -> Bool {
+    guard let str_0 = strs.first  else{
+        return false
+    }
+    
+    let flag_prefix = str_0.prefix(flag)
+    
+    for str in strs {
+        if str.prefix(flag) != flag_prefix {
+            return false
+        }
+    }
+    
+    return true
+}
+
+
+let array = ["flowerdsadadada","flightsss","flight"]
 longestCommonPrefix(array)
